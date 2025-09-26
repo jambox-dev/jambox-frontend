062d1c5bb0e88c16c0926b03f52d80668cc7e8b1
ForBai <thelars42@gmail.com>
2025-09-26 23:36:43 +0200

    feat(env): add environment configuration for production and development

--- src/app/core/services/completion.service.ts
+++ src/app/core/services/completion.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompletionService {
 
-  private apiUrl = 'http://localhost:8080/completion';
+  private apiUrl = `${environment.apiUrl}/completion`;
 
   constructor(private http: HttpClient) { }
 
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:8080' // TODO: Update with actual production API URL
+};
