7f7a39351b16864c94ccc37ceb861c67804e1b44
ForBai <thelars42@gmail.com>
2025-09-27 17:09:51 +0200

    fix(env): update apiUrl to use production API endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080' // TODO: Update with actual production API URL
+  apiUrl: 'https://api.jambox.wiegandt.tech' // TODO: Update with actual production API URL
 };
