8378ae47906db16d2e44b75a084e5c1d67cd5d7c
ForBai <thelars42@gmail.com>
2025-09-27 17:27:22 +0200

    fix(env): correct apiUrl for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.jambox.wiegandt.tech' // TODO: Update with actual production API URL
+  apiUrl: 'https://api-jambox.wiegandt.tech' // TODO: Update with actual production API URL
 };
